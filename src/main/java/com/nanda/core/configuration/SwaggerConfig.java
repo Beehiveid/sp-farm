6c191ca4a89918166fd73a553d0a00125e914dc3
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2020-01-05 15:38:51 +0700

    apply swagger info

--- src/main/java/com/nanda/core/configuration/SwaggerConfig.java
+++ src/main/java/com/nanda/core/configuration/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -16,6 +20,16 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any()).build();
+                .paths(PathSelectors.any()).build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Core API",
+                "Base functional code for web service development",
+                "1.0.0",
+                "Terms of service",
+                new Contact("Febrinanda Endriz Pratama", "https://github.com/febriinanda", "febriinanda@gmail.com"),
+                "License of API", "API license URL", new ArrayList<>());
     }
 }
